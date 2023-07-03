f15ed414eb2649e39fe86978d323de9daa2d3c87
valteu <mail@valteu.com>
2023-07-03 23:49:05 +0200

    regrouped backpropagation in seperate function

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 
 int NUM_LAYERS = 4;
 float LEARNING_RATE = 0.1;
-int BATCHES = 10;
+int BATCHES = 100;
 int BATCHSIZE = 1000;
+int SAMPLESIZE = 1000;
 
 double dataFunction(double x){ // intervall [0, 6] interesting
   return sin(x) + 0.5 * cos(2 * x) + 0.3 * sin(3*x) + 0.2 * cos(4* x) + 0.1 * sin(5*x) + 0.1 * cos(6*x);
@@ -30,12 +31,40 @@ double lossFunction(double data, double* nn_out, int data_idx){
   return loss;
 }
 
+void backpropagation(Layer *Layers[], double* tdata, int sample){
+  Layer *Out = Layers[NUM_LAYERS - 1];
+  for (int o_neuron = 0; o_neuron < Layers[NUM_LAYERS-1]->numNeurons; ++o_neuron){
+    Out->delta[o_neuron] = 2 * (dataFunction(tdata[sample]) - Out->a_output[o_neuron]) * Out->a_output[o_neuron] * (1 - Out->a_output[o_neuron]); // delta for output neuron
+  }
+    for (int layer = NUM_LAYERS - 1; layer > 0; --layer){ //iterates each layer
+      Layer *curr = Layers[layer];
+      Layer *prev = Layers[layer - 1]; //prev is previous layer in list and forward path but next layer in iteraion
+
+      for (int neuron = 0; neuron < prev->numNeurons; ++neuron){ //calculate delta for prev layer
+        prev->delta[neuron] = 0.0;
+        for (int curr_neurons = 0; curr_neurons < curr->numNeurons; ++curr_neurons){
+         prev->delta[neuron] += curr->delta[curr_neurons] * curr->weights[curr_neurons][neuron] * prev->a_output[neuron] * (1 - prev->a_output[neuron]);
+       } 
+      }
+    }
+  
+  for (int layer = NUM_LAYERS; layer > 0; --layer){
+    for (int neuron = 0; neuron < Layers[layer]->numNeurons; ++neuron){
+      Layers[layer]->biases[neuron] -= LEARNING_RATE * Layers[layer]->gradientBiases[neuron];
+      for (int in_size = 0; in_size < Layers[layer]->inputSize; ++in_size){
+        Layers[layer]->weights[neuron][in_size] -= LEARNING_RATE * Layers[layer]->gradientWeights[neuron][in_size];
+      }
+    }
+  }
+}
+
 int main(){
   // init
   double* tdata;
   double loss;
+  tdata = createData(BATCHSIZE);
 
-  Layer In(1, BATCHES, 'I');
+  Layer In(1, 1, 'I');
   Layer *pIn = &In;
   Layer H1(1, 4, '1');
   Layer *pH1 = &H1;
@@ -46,12 +75,11 @@ int main(){
 
   Layer *Layers[] = {pIn, pH1, pH2, pOut};
 
-  for (int batch = 0; batch < BATCHES; ++batch){
-  tdata = createData(BATCHSIZE);
-  In.a_output = tdata;
+  for (int epoch = 0; epoch< 1000; ++epoch){
   loss = 0;
   // Forward
-  for (int sample = 0; sample < BATCHSIZE; ++sample){
+  for (int sample = 0; sample < SAMPLESIZE; ++sample){
+      In.a_output = tdata;
       H1.forward(tdata);
       H1.sigmoid(H1.f_output);
 
@@ -65,36 +93,9 @@ int main(){
     printf("Loss: %lf\n", loss / sample + 1);
 
     // Backpropagation
-    for (int o_neuron = 0; o_neuron < pOut->numNeurons; ++o_neuron){
-      Out.delta[o_neuron] = 2 * (dataFunction(tdata[sample]) - pOut->a_output[o_neuron]) * pOut->a_output[o_neuron] * (1 - pOut->a_output[o_neuron]); // delta for output neuron
-      }
-    for (int layer = NUM_LAYERS - 1; layer > 0; --layer){ //iterates each layer
-      Layer *curr = Layers[layer];
-      Layer *prev = Layers[layer - 1]; //prev is previous layer in list and forward path but next layer in iteraion
+    backpropagation(Layers, tdata, sample);
+    }
+  }
 
-      for (int neuron = 0; neuron < curr->numNeurons; ++neuron){ // update weights
-        for (int inp = 0; inp < curr->inputSize; ++inp){
-          curr->gradientWeights[neuron][inp] += curr->delta[neuron]* prev->a_output[neuron]; 
-        }
-        curr->gradientBiases[neuron] += curr->delta[neuron]; // update bias
-      }
-      for (int neuron = 0; neuron < prev->numNeurons; ++neuron){ //calculate delta for prev layer
-        prev->delta[neuron] = 0.0;
-        for (int curr_neurons = 0; curr_neurons < curr->numNeurons; ++curr_neurons){
-         prev->delta[neuron] += curr->delta[curr_neurons] * curr->weights[curr_neurons][neuron] * prev->a_output[neuron] * (1 - prev->a_output[neuron]);
-       } 
-      }
-    }
-  }
-  loss /= BATCHSIZE;
-  for (int layer = NUM_LAYERS; layer > 0; --layer){
-    for (int neuron = 0; neuron < Layers[layer]->numNeurons; ++neuron){
-      Layers[layer]->biases[neuron] -= LEARNING_RATE * Layers[layer]->gradientBiases[neuron] / BATCHSIZE;
-      for (int in_size = 0; in_size < Layers[layer]->inputSize; ++in_size){
-        Layers[layer]->weights[neuron][in_size] -= LEARNING_RATE * Layers[layer]->gradientWeights[neuron][in_size] / BATCHSIZE;
-      }
-    }
-  }
-}
   return 0;
 }
